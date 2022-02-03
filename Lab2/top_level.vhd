e0fb6e434eee4e4ed8159736c4ffda632010d53e
aarjones <89935624+aarjones@users.noreply.github.com>
2022-02-03 14:24:17 -0500

    Added i2c to top_level

--- Lab2/top_level.vhd
+++ Lab2/top_level.vhd
@@ -93,6 +93,24 @@ architecture behavioral of top_level is
 			    rs    : out std_logic                      --to LCD
 		  ); 
 	end component;
+	
+	component i2c_master is
+	  generic(
+		 input_clk : INTEGER := 100_000_000; --input clock speed from user logic in Hz
+		 bus_clk   : INTEGER := 400_000);   --speed the i2c bus (scl) will run at in Hz
+	  port(
+		 clk       : IN     STD_LOGIC;                    --system clock
+		 reset_n   : IN     STD_LOGIC;                    --active low reset
+		 ena       : IN     STD_LOGIC;                    --latch in command
+		 addr      : IN     STD_LOGIC_VECTOR(6 DOWNTO 0); --address of target slave
+		 rw        : IN     STD_LOGIC;                    --'0' is write, '1' is read
+		 data_wr   : IN     STD_LOGIC_VECTOR(7 DOWNTO 0); --data to write to slave
+		 busy      : OUT    STD_LOGIC;                    --indicates transaction in progress
+		 data_rd   : OUT    STD_LOGIC_VECTOR(7 DOWNTO 0); --data read from slave
+		 ack_error : BUFFER STD_LOGIC;                    --flag if improper acknowledge from slave
+		 sda       : INOUT  STD_LOGIC;                    --serial data output of i2c bus
+		 scl       : INOUT  STD_LOGIC);                   --serial clock output of i2c bus
+	end i2c_master;
 		
 begin
 --INSTANTIATIONS
