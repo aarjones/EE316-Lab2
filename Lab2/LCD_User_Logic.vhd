1cc0a328c21d9c553b3e82bdca948ec66d610704
aarjones <89935624+aarjones@users.noreply.github.com>
2022-02-03 16:17:41 -0500

    Modified LCD_User_Logic

--- Lab2/LCD_User_Logic.vhd
+++ Lab2/LCD_User_Logic.vhd
@@ -3,8 +3,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 
 entity LCD_User_Logic is
-    Port ( iClk : in STD_LOGIC; -- 125 MHz    
+    Port ( iClk : in STD_LOGIC; -- 50 MHz    
            reset : in std_logic;
+			  speed_sel : in std_logic_vector(1 downto 0); --60, 120, or 1000 Hz
            Data : out STD_LOGIC_VECTOR (7 downto 0); -- to LCD
            en   : out STD_LOGIC;                     --to LCD
            rs   : out STD_LOGIC                      --to LCD
@@ -34,7 +35,6 @@ signal currentByte : std_logic_vector(7 downto 0);
 signal currentByte_wr : std_logic_vector(7 downto 0);
 signal RS_sig : std_logic;
 signal currentRS : std_logic;
-signal clk_en : std_logic;
 signal byteSel : integer range 0 to 29 := 0;
 signal count : unsigned(27 DOWNTO 0):=X"000000F";
 signal ena : std_logic;
@@ -57,36 +57,99 @@ Inst_LCD_Transmitter : LCD_Transmitter
 process(byteSel)
     begin
         case byteSel is
-            when 0  => currentByte <= X"38"; RS_sig <= '0';
-            when 1  => currentByte <= X"38"; RS_sig <= '0';
-            when 2  => currentByte <= X"38"; RS_sig <= '0';
-            when 3  => currentByte <= X"38"; RS_sig <= '0';
-            when 4  => currentByte <= X"38"; RS_sig <= '0';
-            when 5  => currentByte <= X"38"; RS_sig <= '0';
-            when 6  => currentByte <= X"01"; RS_sig <= '0';
-            when 7  => currentByte <= X"0C"; RS_sig <= '0';
-            when 8  => currentByte <= X"06"; RS_sig <= '0';
-            when 9  => currentByte <= X"80"; RS_sig <= '0';
-            when 10 => currentByte <= X"53"; RS_sig <= '1';
-            when 11 => currentByte <= X"79"; RS_sig <= '1';
-            when 12 => currentByte <= X"73"; RS_sig <= '1';
-            when 13 => currentByte <= X"74"; RS_sig <= '1';
-            when 14 => currentByte <= X"65"; RS_sig <= '1';
-            when 15 => currentByte <= X"6D"; RS_sig <= '1';
-            when 16 => currentByte <= X"FE"; RS_sig <= '1';
-            when 17 => currentByte <= X"52"; RS_sig <= '1';
-            when 18 => currentByte <= X"65"; RS_sig <= '1';
-            when 19 => currentByte <= X"61"; RS_sig <= '1';
-            when 20 => currentByte <= X"64"; RS_sig <= '1';
-            when 21 => currentByte <= X"79"; RS_sig <= '1';
-            when 22 => currentByte <= X"C0"; RS_sig <= '0';--REPEAT FROM HERE
-            when 23 => currentByte <= X"3D"; RS_sig <= '1';
-            when 24 => currentByte <= X"3D"; RS_sig <= '1';
-            when 25 => currentByte <= X"3E"; RS_sig <= '1';
-            when 26 => currentByte <= X"44"; RS_sig <= '1';
-            when 27 => currentByte <= X"45"; RS_sig <= '1';
-            when 28 => currentByte <= X"41"; RS_sig <= '1';
-            when 29 => currentByte <= X"44"; RS_sig <= '1';
+            when 0  => currentByte <= X"00"; RS_sig <= '0'; --clear display                  --START INT
+            when 1  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 2  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 3  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 4  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 5  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 6  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 7  => currentByte <= X"00"; RS_sig <= '0'; --clear display
+            when 8  => currentByte <= X"02"; RS_sig <= '0'; --return home
+            when 9  => currentByte <= X"04"; RS_sig <= '0'; --display on, cursor off, cursor blink off
+            when 10 => currentByte <= X"49"; RS_sig <= '1'; -- "I" 
+            when 11 => currentByte <= X"6E"; RS_sig <= '1'; -- "n"
+            when 12 => currentByte <= X"69"; RS_sig <= '1'; -- "i"
+            when 13 => currentByte <= X"74"; RS_sig <= '1'; -- "t"
+            when 14 => currentByte <= X"69"; RS_sig <= '1'; -- "i"
+            when 15 => currentByte <= X"61"; RS_sig <= '1'; -- "a"
+            when 16 => currentByte <= X"6C"; RS_sig <= '1'; -- "l"
+            when 17 => currentByte <= X"69"; RS_sig <= '1'; -- "i"
+            when 18 => currentByte <= X"7A"; RS_sig <= '1'; -- "z"
+            when 19 => currentByte <= X"69"; RS_sig <= '1'; -- "i"
+            when 20 => currentByte <= X"6E"; RS_sig <= '1'; -- "n"
+            when 21 => currentByte <= X"67"; RS_sig <= '1'; -- "g"                           --END INIT
+				when 22 => currentByte <= X"00"; RS_sig <= '0'; -- clear display                 --START TEST MODE
+				when 23 => currentByte <= X"80"; RS_sig <= '0'; -- send curor to top row
+            when 24 => currentByte <= X"54"; RS_sig <= '1'; -- "T"  
+            when 25 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+            when 26 => currentByte <= X"73"; RS_sig <= '1'; -- "s"
+            when 27 => currentByte <= X"74"; RS_sig <= '1'; -- "t"
+            when 28 => currentByte <= X"20"; RS_sig <= '1'; -- " "
+            when 29 => currentByte <= X"4D"; RS_sig <= '1'; -- "M"
+            when 30 => currentByte <= X"6F"; RS_sig <= '1'; -- "o"
+            when 31 => currentByte <= X"64"; RS_sig <= '1'; -- "d"
+				when 32 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+				when 33 => currentByte <= X"C0"; RS_sig <= '0'; -- Move to second row, first char --START PAUSE
+            when 34 => currentByte <= X"30"; RS_sig <= '1'; -- "0"
+            when 35 => currentByte <= X"78"; RS_sig <= '1'; -- "x"
+            when 36 => currentByte <= X""; RS_sig <= '1'; -- data left
+            when 37 => currentByte <= X""; RS_sig <= '1'; -- data left middle
+            when 38 => currentByte <= X""; RS_sig <= '1'; -- data right middle
+            when 39 => currentByte <= X""; RS_sig <= '1'; -- data right 
+				when 40 => currentByte <= X"20"; RS_sig <= '1'; -- " "
+				when 41 => currentByte <= X"61"; RS_sig <= '1'; -- "a"
+            when 42 => currentByte <= X"74"; RS_sig <= '1'; -- "t"
+            when 43 => currentByte <= X"20"; RS_sig <= '1'; -- " "
+            when 44 => currentByte <= X"30"; RS_sig <= '1'; -- "0"
+            when 45 => currentByte <= X"78"; RS_sig <= '1'; -- "x"
+            when 46 => currentByte <= X""; RS_sig <= '1'; -- address left
+            when 47 => currentByte <= X""; RS_sig <= '1'; -- address right                    --END TEST MODE
+				when 48 => currentByte <= X"00"; RS_sig <= '0'; -- clear display
+				when 49 => currentByte <= X"80"; RS_sig <= '0'; -- send cursor to top row
+				when 50 => currentByte <= X"50"; RS_sig <= '1'; -- "P"  
+            when 51 => currentByte <= X"61"; RS_sig <= '1'; -- "a"
+            when 52 => currentByte <= X"75"; RS_sig <= '1'; -- "u"
+            when 53 => currentByte <= X"73"; RS_sig <= '1'; -- "s"
+            when 54 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+				when 55 => currentByte <= X"20"; RS_sig <= '1'; -- " "
+            when 56 => currentByte <= X"4D"; RS_sig <= '1'; -- "M"
+            when 57 => currentByte <= X"6F"; RS_sig <= '1'; -- "o"
+            when 58 => currentByte <= X"64"; RS_sig <= '1'; -- "d"
+				when 59 => currentByte <= X"65"; RS_sig <= '1'; -- "e"                           --END PAUSE MODE
+				when 60 => currentByte <= X"00"; RS_sig <= '0'; -- clear display                 --START PWM GEN
+				when 61 => currentByte <= X"80"; RS_sig <= '0'; -- cursor top row
+            when 62 => currentByte <= X"47"; RS_sig <= '1'; -- "G"
+            when 63 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+            when 64 => currentByte <= X"6E"; RS_sig <= '1'; -- "n"
+            when 65 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+				when 66 => currentByte <= X"72"; RS_sig <= '1'; -- "r"
+            when 67 => currentByte <= X"61"; RS_sig <= '1'; -- "a"
+            when 68 => currentByte <= X"74"; RS_sig <= '1'; -- "t"
+            when 69 => currentByte <= X"69"; RS_sig <= '1'; -- "i"
+				when 70 => currentByte <= X"6E"; RS_sig <= '1'; -- "n"
+            when 71 => currentByte <= X"67"; RS_sig <= '1'; -- "g"
+            when 72 => currentByte <= X"20"; RS_sig <= '1'; -- " "
+            when 73 => currentByte <= X"50"; RS_sig <= '1'; -- "P"
+				when 74 => currentByte <= X"57"; RS_sig <= '1'; -- "W"
+            when 75 => currentByte <= X"4D"; RS_sig <= '1'; -- "M"
+            when 76 => currentByte <= X"C0"; RS_sig <= '0'; -- cursor bottom row
+            when 77 => currentByte <= X""; RS_sig <= '1'; -- "6"
+				when 78 => currentByte <= X"47"; RS_sig <= '1'; -- "G"
+            when 79 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+            when 80 => currentByte <= X"6E"; RS_sig <= '1'; -- "n"
+            when 81 => currentByte <= X"65"; RS_sig <= '1'; -- "e"
+				when 82 => currentByte <= X"72"; RS_sig <= '1'; -- "r"
+            when 83 => currentByte <= X"61"; RS_sig <= '1'; -- "a"
+            when 84 => currentByte <= X"74"; RS_sig <= '1'; -- "t"
+            when 85 => currentByte <= X"69"; RS_sig <= '1'; -- "i"
+				when 86 => currentByte <= X"6E"; RS_sig <= '1'; -- "n"
+            when 87 => currentByte <= X"67"; RS_sig <= '1'; -- "g"
+            when 88 => currentByte <= X"20"; RS_sig <= '1'; -- " "
+            when 89 => currentByte <= X"50"; RS_sig <= '1'; -- "P"
+				when 90 => currentByte <= X"57"; RS_sig <= '1'; -- "W"
+            when 91 => currentByte <= X"4D"; RS_sig <= '1'; -- "M"
+				
         end case;
 end process;
 
--- Lab2/system_controller.vhd
+++ Lab2/system_controller.vhd
@@ -5,16 +5,12 @@ use ieee.numeric_std.all;
 entity system_controller is
 	port(
 		clk          : in    std_logic;                     --50 MHz
-		clk_en_200   : in    std_logic;                     --200 MHz, from keypad
-		kp_value     : in    std_logic_vector(4 downto 0);  --HEX value of the keypad
-		keypad_valid : in    std_logic;                     --is the keypad value valid?
 		reset_h      : in    std_logic;                     --active-high reset
 		SRAM_busy_h  : in    std_logic;                     --is the SRAM controller busy?
 
-		mode_LED     : out   std_logic;                     --1 in operation, 0 in programming
-		data_select  : out   std_logic_vector(1 downto 0);  --1 is ROM, 0 is manual
 		data_i       : in    std_logic_vector(15 downto 0); --16-bit data
 		data_o       : out   std_logic_vector(15 downto 0); --16-bit data
+		data_select  : out   std_logic;                     --data select for multiplexor
 		address_out  : out   std_logic_vector(7 downto 0);  --8-bit address
 		SRAM_rw      : out   std_logic;                     --1 is read, 0 is write
 		SRAM_valid   : out   std_logic                      --is our data valid to be computed by the SRAM
@@ -22,38 +18,21 @@ entity system_controller is
 end system_controller;
 
 architecture behavioral of system_controller is
-	type stateType is (init, operation_1hz, operation_stop, program_data, program_address);
+	type stateType is (init, test, pause, pwm60, pwm120, pwm1000);
 	
 	--For State Machine
 	signal state, next_state : stateType                     := init;
 	signal ROM_cnt           : integer range 0 to 256        := 0;
 	signal address_cnt       : integer range 0 to 255        := 0;
+	signal to_increment      : std_logic                     := '0'; --does the ROM need to be incremented
 	signal SRAM_rw_int       : std_logic                     := '0'; --0 is write, 1 is read
-	signal data_io_out       : std_logic_vector(15 downto 0);        --what will be written to the SRAM
-	signal up                : std_logic                     := '1'; --1 is counting up, 2 is down
-	signal data_int          : std_logic_vector(15 downto 0);        --read data, internal
-	signal address_int       : std_logic_vector(7 downto 0);         --address, internal
-	signal sampled           : std_logic;                            --have we sampled from data_i?
-	signal SRAM_valid_o      : std_logic;                            --is the SRAM_io valid?
-	signal to_increment      : std_logic;
-	
-	--For command controller
-	signal program_SRAM      : std_logic                     := '0'; --trigger a pulse down SRAM_valid
-	signal read_SRAM         : std_logic                     := '0'; --trigger a pulse down SRAM_valid
-	
-	--For clk_en_1
-	signal clk_cnt           : integer range 0 to 50000000-1 := 0;
-	signal clk_en_1          : std_logic                     := '0';
-	
-	--For 1 HZ counter
-	signal run_counter       : std_logic                     := '0'; --should the counter run?
+	signal SRAM_valid_int    : std_logic;                            --is the SRAM_io valid?
 	
 	begin
 	
 	state <= next_state;
 	SRAM_rw <= SRAM_rw_int;
-	data_o <= data_io_out;
-	SRAM_valid <= SRAM_valid_o;
+	SRAM_valid <= SRAM_valid_int;
 	
 	--Process for state machine
 	process(clk) begin
@@ -62,172 +41,52 @@ architecture behavioral of system_controller is
 			case(state) is 
 				when init =>
 					SRAM_rw_int <= '0';                                       --we're writing
-					data_select <= "01";                                      --reading from ROM
-					mode_LED    <= '0';                                       --programming
-					run_counter <= '0';                                       --keep the counter off
+					data_select <= '0';                                       --reading from ROM
 					address_out <= std_logic_vector(to_unsigned(ROM_cnt, 8)); --convert the ROM_cnt to address
 					if SRAM_busy_h = '0' then                                 --if the controller is ready
+						SRAM_valid_o <= '1';                                  --take in the next value
 						if to_increment = '1' then
 							ROM_cnt <= ROM_cnt + 1;
 							to_increment <= '0';
 						end if;
-						SRAM_valid_o <= '1';                                  --take in the next value
+						
 					elsif SRAM_valid_o = '1' then
-							SRAM_valid_o <= '0';                              --take valid back low
-							to_increment <= '1';                              --prepare for next count
+							SRAM_valid_o <= '0';                               --take valid back low
+							to_increment <= '1';                               --prepare for next count
 					end if;
-					if ROM_cnt = 256 then                                     --if we've reached the end\
-						ROM_cnt <= 0;
-						next_state <= operation_stop;                         --move to the next state
+					if ROM_cnt = 256 then                                    --if we've reached the end
+						ROM_cnt     <= 0;
+						next_state  <= test;                                   --move to the next state
 						SRAM_rw_int <= '1';
 						address_cnt <= 0;
-						read_SRAM <= '1';
-						data_io_out <= (others => '0');
-					end if;		
+						read_SRAM   <= '1';
+						data_io_o   <= (others => '0');
+					end if;	
 					
-				when operation_1hz =>
-					mode_LED    <= '1';  --we are in operation mode
-					data_select <= "00"; --we are controlling data to SRAM
-					SRAM_rw_int <= '1';  --we are reading
-					run_counter <= '1';  --turn on the counter
-					address_out <= std_logic_vector(to_unsigned(address_cnt, 8));
-					data_io_out <= data_i;
---					if sampled = '0' then
---						if SRAM_busy_h = '0' then
---							sampled <= '1';
---						end if;
---					end if;
-					if read_SRAM = '1' then
-						sampled <= '0';
-						SRAM_valid_o <= '1';
-						read_SRAM <= '0';
-					else
-						SRAM_valid_o   <= '0';
-					end if;
+				when test =>
 					
-				when operation_stop =>
-					mode_LED    <= '1';  --we are in operation mode
-					data_select <= "00"; --we are controlling data to SRAM
-					SRAM_rw_int <= '1';  --we are reading
-					run_counter <= '0';  --turn off the counter
-					address_out <= std_logic_vector(to_unsigned(address_cnt, 8));
-					data_io_out <= data_i;
---					if sampled = '0' then
---						if SRAM_busy_h = '0' then
---							sampled <= '1';
---						end if;
---					end if;
-					if read_SRAM = '1' and SRAM_busy_h = '0' then
-						sampled <= '0';
-						SRAM_valid_o <= '1';
-						read_SRAM <= '0';
-					elsif SRAM_busy_h = '1' and sampled = '0' then
-						SRAM_valid_o   <= '0';
-					end if;
 					
-				when program_data =>
-					mode_LED    <= '0';
-					data_select <= "10";
-					SRAM_rw_int <= '0';
-					address_out <= address_int;
-					if program_SRAM = '1' then
-						SRAM_valid_o   <= '1';
-						program_SRAM <= '0';
-					else
-						SRAM_valid_o   <= '0';
-					end if;
+				when pause =>
 					
-				when program_address =>
-					mode_LED    <= '0';
-					data_select <= "10";
-					SRAM_rw_int <= '0';
-					address_out <= address_int;
-					if program_SRAM = '1' then
-						SRAM_valid_o   <= '1';
-						program_SRAM <= '0';
-					else
-						SRAM_valid_o   <= '0';
-					end if;
+					
+				when pwm60 =>
+					
+					
+				when pwm120 =>
+					
+				
+				when pwm1000 =>
+					
 					
 			end case;
 		elsif reset_h = '1' then
-			next_state <= init;
-			ROM_cnt <= 0;
-			address_cnt <= 0;
-			address_int <= (others => '0');
-			up <= '1';
-			clk_cnt <= 0;
-			run_counter <= '0';
-			data_select <= "01";
-			data_io_out <= (others => '0');
-			read_SRAM <= '1';
-			SRAM_valid_o <= '0';
-		end if;
-	
-	--Process for normal keypresses
-		if clk_en_200 = '1' and keypad_valid = '1' and kp_value(4) = '0' then --If the keypad has a good, non-command output
-			case(state) is                                                    --complete relevant shift register operation
-				when program_data    => data_io_out(3 downto 0) <= kp_value(3 downto 0); data_io_out(15 downto 4) <= data_io_out(11 downto 0);
-				when program_address => address_int(3 downto 0) <= kp_value(3 downto 0); address_int(7  downto 4) <= address_int(3  downto 0);
-				when others => null;
-			end case;
-		end if;
-	
-	--Process for commands
-		if clk_en_200 = '1' and keypad_valid = '1' then --If the keypad has a good output
-			case(kp_value) is
-				when "10000" =>                         --H was pressed
-					case(state) is
-						when program_address => next_state <= program_data;
-						when program_data    => next_state <= program_address;
-						when operation_1hz   => next_state <= operation_stop;
-						when operation_stop  => next_state <= operation_1hz; read_SRAM <= '1';
-						when others          => null;
-					end case;
-				when "10010" =>                         --Shift was pressed
-					case(state) is                      --Change program/operation
-						when operation_1hz   | operation_stop => next_state <= program_address; address_int <= (others => '0'); data_io_out <= (others => '0');
-						when program_address | program_data   => next_state <= operation_stop;  address_cnt <= 0; SRAM_rw_int <= '1'; data_io_out <= (others => '0'); read_SRAM <= '1'; up <= '1';
-						when others                           => null;
-					end case;
-				when "10001" =>                         --L was pressed
-					case(state) is
-						when program_address | program_data   => program_SRAM <= '1'; sampled <= '0';
-						when operation_1hz   | operation_stop => up <= not up;
-						when others => null;
-					end case;
-				when others => null;                    --Anything else (0-F)
-			end case;
-		end if;
-	
-	--Process for 1 Hz clock enable
-			if clk_cnt = 50000000-1 then -- change to 50000000 for board
-				clk_cnt <= 0;
-				clk_en_1 <= '1';
-			else
-				clk_cnt <= clk_cnt + 1;
-				clk_en_1 <= '0';
-			end if;
-	
-	--Process for 1 Hz operation counter
-		if run_counter = '1' then
-			if SRAM_busy_h = '1' then
-				SRAM_valid_o <= '0';
-			elsif clk_en_1 = '1' then
-				read_SRAM <= '1';
-				if up = '1' then
-					address_cnt <= address_cnt + 1;
-					if address_cnt = 255 then
-						address_cnt <= 0;
-					end if;
-				else
-					address_cnt <= address_cnt - 1;
-					if address_cnt = 0 then
-						address_cnt <= 255;
-					end if;
-				end if;
-			end if;
-		end if;
+			next_state <= init;             --move to init
+			data_o <= (others => '0');      --reset to 0s
+			address_out <= (others => '0'); --reset to 0s
+			SRAM_rw => '1';                 --reading
+			SRAM_valid_int => '0';          --reset to 0
+			ROM_cnt => 0;                   --reset to 0
+			address_cnt => 0;               --reset to 0
 		end if;
 	end process;
 end behavioral;
